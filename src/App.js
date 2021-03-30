64154499e3d45a28e0bd54d6288dcbc89a57bf80
Herman <herman@slyngstadli.no>
2021-03-30 21:50:22 +0200

    la inn tekst

--- src/App.js
+++ src/App.js
@@ -1,8 +1,34 @@
+import styled from 'styled-components'
+import './App.css'
+
+const Wrapper = styled.div`
+    display: grid;
+    grid-template-columns: repeat(12,1fr);
+    grid-gap: 16px;
+    gap: 16px;
+    padding: 0 64px;
+    max-width: 1600px;
+    margin: 0 auto;
+	  @media only screen and (max-width: 850px) {
+      padding:0 24px;
+	  }
+`;
+
+const GridItem = styled.div`
+  margin-bottom: 3rem;
+  text-align: center;
+  padding-top:10em;
+	grid-column: ${(props) => props.large};
+    @media only screen and (max-width: 850px) {
+        grid-column: ${(props) => props.small};
+	}
+`;
+
 function App() {
   return (
-    <div>
-      Herman Slyngstadli
-    </div>
+    <Wrapper>
+      <GridItem small="1 / 12" large="1 / 12">Herman Slyngstadli</GridItem>
+    </Wrapper>
   );
 }
 
